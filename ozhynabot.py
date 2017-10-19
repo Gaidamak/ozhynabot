6ff5c4505883d94a6ffb55d2a5175da2cb0a36e0
Pasha Haidamak <haidamak@macpaw.com>
2017-10-19 14:35:04 +0300

    token added to the URL

--- ozhynabot.py
+++ ozhynabot.py
@@ -5,7 +5,7 @@ class BotHandler:
 
     def __init__(self, token):
         self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.api_url = "https://api.telegram.org/bot473588601:AAFxZPg3l_HcwESu72G2mAgYzIp9qmEqf8U/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
